a1115a944657d4f6ecc3e8ec21ce336226f4800d
machavarian10to <otomachavariani24@gmail.com>
2022-07-05 20:40:09 +0400

    experience page fields without styling and with functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const stepOne = document.getElementById('one');
+const stepTwo = document.getElementById('two');
+
 const userName = document.getElementById('user-name');
 const userEmail = document.getElementById('user-email');
 const userPhone = document.getElementById('user-phone');
@@ -18,9 +21,18 @@ const emailErrClose = document.getElementById("email-err-close");
 const phoneErrClose = document.getElementById("phone-err-close");
 const dateErrClose = document.getElementById("date-err-close");
 
-const stepOne = document.getElementById('one');
+const knowledge = document.getElementById('knowledge-level');
+const character = document.getElementById('characters');
+// const experience = document.getElementById('experience')
+const championship = document.getElementsByName('redberry-championship');
+
+
+const knowledgeError = document.getElementById('knowledge-error');
+const characterError = document.getElementById('character-error')
+const experienceError = document.getElementById('experience-error')
 
 const nextButton = document.getElementById('next-btn');
+const doneButton = document.getElementById('done-btn')
 
 usernameErrClose.addEventListener('click', () => {
     usernameError.style.display = 'none'
@@ -38,8 +50,43 @@ dateErrClose.addEventListener('click', () => {
     dateError.style.display = 'none'
 })
 
+https://chess-tournament-api.devtest.ge/images/nona.jpg
 
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdown = document.getElementById('characters');
 
+    fetch('https://chess-tournament-api.devtest.ge/api/grandmasters')
+    .then(res => res.json())
+    .then(characters => {
+        let options = '';
+        characters.forEach(character => {
+            options += `<option value='${character.name}'>${character.name}</option>`
+        });
+        options += `<option value='other'>Other</option>`
+        dropdown.innerHTML += options;
+    })
+    .catch(err => console.log(err))
+})
+
+function registrationStarted() {
+    if(userName.value.length > 0 || userEmail.value.length > 0 || userPhone.value.length > 0 || userBirthday.value.length > 0) {
+        stepOne.style.backgroundColor = '#E9FAF1';
+        stepOne.style.border = 'none';
+    }else{
+        stepOne.style.backgroundColor = 'transparent';
+        stepOne.style.border = '1px solid #E5E6E8';
+    }
+}
+
+function registrationContinue() {
+    if(knowledge.value.length > 0 || character.value.length > 0){
+        stepTwo.style.backgroundColor = '#E9FAF1';
+        stepTwo.style.border = 'none';
+    }else{
+        stepTwo.style.backgroundColor = 'transparent';
+        stepTwo.style.border = '1px solid #E5E6E8';
+    }
+}
 
 function userInputsValidation() {
     if(nameValidation() && emailValidation() && phoneValidation() &&  birthdayValidation()){
@@ -52,22 +99,48 @@ function userInputsValidation() {
     birthdayValidation();
 }
 
-function registrationStarted() {
-    if(userName.value.length > 0 || userEmail.value.length > 0 || userPhone.value.length > 0 || userBirthday.value.length > 0) {
-        stepOne.style.backgroundColor = '#E9FAF1'
-        stepOne.style.border = 'none';
-    }else{
-        stepOne.style.backgroundColor = 'transparent'
-        stepOne.style.border = '1px solid #E5E6E8';
+function userExperiencesValidation() {
+    if(knowledgeValidation() && characterValidation() && experienceValidation()){
+        doneButton.onclick = window.location.href='/onboarding.html'
     }
+    knowledgeValidation();
+    characterValidation();
+    experienceValidation();
+}
+
+function knowledgeValidation() {
+    if(knowledge.value === 'title'){
+        knowledgeError.style.display = 'block'
+        return false;
+    }
+    knowledgeError.style.display = 'none'
+    return true;
+}
+
+function characterValidation() {
+    if(character.value === 'title'){
+        characterError.style.display = 'block'
+        return false;
+    }
+    characterError.style.display = 'none'
+    return true;
+}
+
+function experienceValidation() {
+    if(!championship[0].checked && !championship[1].checked){
+        experienceError.style.display = 'block'
+        return false;
+    }
+    experienceError.style.display = 'none'
+    return true;
 }
 
 function nameValidation() {
-    if(userName.value.length === 0 || userName.value.length < 2) {
-        nameCheck.innerHTML = ''
+    if(userName.value.length < 2) {
+        nameCheck.innerHTML = '';
+        usernameError.style.display = 'block'
         userName.style.background = '#FFEFEF';
         userName.style.color = '#DC3545';
-        usernameError.style.display = 'block'
         return false;
     }
     userName.style.background = '#F8F9FA';
@@ -78,31 +151,31 @@ function nameValidation() {
 }
 
 function emailValidation() {
-    if(userEmail.value.length === 0 || !userEmail.value.includes('@redberry.ge')){
-        nameCheck.innerHTML = ''
+    if(!userEmail.value.includes('@redberry.ge')){
+        emailCheck.innerHTML = '';
+        emailError.style.display = 'block'
         userEmail.style.background = '#FFEFEF';
         userEmail.style.color = '#DC3545';
-        emailError.style.display = 'block'
         return false
     }
-    userName.style.background = '#F8F9FA';
-    userName.style.color = 'black';
-    emailError.style.display = 'block'
+    userEmail.style.background = '#F8F9FA';
+    userEmail.style.color = 'black';
+    emailError.style.display = 'none'
     emailCheck.innerHTML = "<img src='./assets/check.png'/>"
     return true
 }
 
 function phoneValidation() {
-    if(userPhone.value.length === 0 || userPhone.value.length < 9){
+    if(userPhone.value.length < 9){
         phoneCheck.innerHTML = ''
+        phoneError.style.display = 'block'
         userPhone.style.background = '#FFEFEF';
         userPhone.style.color = '#DC3545';
-        phoneError.style.display = 'block'
         return false
     }
     userPhone.style.background = '#F8F9FA';
     userPhone.style.color = 'black';
-    phoneError.style.display = 'block'
+    phoneError.style.display = 'none'
     phoneCheck.innerHTML = "<img src='./assets/check.png'/>"
     return true
 }
@@ -110,18 +183,16 @@ function phoneValidation() {
 
 function birthdayValidation() {
     if(userBirthday.value.length === 0){
-        phoneCheck.innerHTML = ''
+        birthdayCheck.innerHTML = ''
+        dateError.style.display = 'block'
         userBirthday.style.background = '#FFEFEF';
         userBirthday.style.color = '#DC3545';
-        dateError.style.display = 'block'
         return false
     }
     userBirthday.style.background = '#F8F9FA';
     userBirthday.style.color = 'black';
-    dateError.style.display = 'block'
-    birthdayCheck.innerHTML = "<img src='./assets/check.png'/>"
+    dateError.style.display = 'none';
+    birthdayCheck.innerHTML = "<img src='./assets/check.png'/>";
     return true
 }
-
-
 
